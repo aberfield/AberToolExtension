918965cbe0f1e49bf4058056229cd5a9161c3622
aberfield <liufangyou521@163.com>
2018-08-29 15:56:42 +0800

    init library

---
+++ Example/Pods/Target Support Files/AberToolExtension/AberToolExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AberToolExtension : NSObject
+@end
+@implementation PodsDummy_AberToolExtension
+@end
---
+++ Example/Pods/Target Support Files/Pods-AberToolExtension_Tests/Pods-AberToolExtension_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AberToolExtension_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AberToolExtension_Tests
+@end
