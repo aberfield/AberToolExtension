b05a63361b437bf9472db7c749a949f3231980f1
aberfield <liufangyou521@163.com>
2019-05-31 00:39:45 +0800

    feat: 测试 gitflow

--- Example/AberToolExtension/ViewController.swift
+++ Example/AberToolExtension/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         var a = [1,2,1,2,3,4,5]
         a.removeDuplicates()
         print(a)
-
+        print(a)
     }
 
     override func didReceiveMemoryWarning() {
